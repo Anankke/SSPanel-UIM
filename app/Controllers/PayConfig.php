637255134835ec7265abb446a467ccd8085a6f96
NimaQu <admin@nimaqu.com>
2018-02-24 12:28:05 +0800

    将易付通key配置写入config

--- app/Controllers/PayConfig.php
+++ app/Controllers/PayConfig.php
@@ -9,8 +9,6 @@ class PayConfig{
     public function init()
     {
         $this->pay_config = [
-            "secret" => "9A30EEBF727DE9A98B6AF916C2BCCA75",
-            "accesskey" => "C390D630A2B48094854048740A3900D9",
             "notify_url" => "/yft/notify",
             "return_url" => "/yft/notify",
             "type" => "aliPay"
