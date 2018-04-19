049aaf63b321ba882de6b56a5c1f31a643ac3a0a
dumplin <1277385101@qq.com>
2018-04-20 00:25:41 +0800

    新增功能：重置所有用户端口 使用方法：php xcat resetport

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -74,6 +74,8 @@ class XCat
                 return Job::updatedownload();
             case("cleanRelayRule"):
                 return $this->cleanRelayRule();
+            case("resetport"):
+                return $this->resetport();
             default:
                 return $this->defaultAction();
         }
@@ -82,6 +84,17 @@ class XCat
     public function defaultAction()
     {
         echo "Memo";
+    }
+
+    public function resetport()
+    {
+        $users = User::all();
+        foreach ($users as $user) {
+            $user = $this->user;
+            $origin_port = $user->port;
+            $user->port = Tools::getAvPort();
+            echo '$origin_port='.$origin_port.'&$user->port='.$user->port.'/n';
+        }
     }
 
     public function cleanRelayRule()
