f6aebad4c3e929f0a5833e65ae8916d48b38905c
aiden3434 <40372263+aiden3434@users.noreply.github.com>
2018-07-01 22:55:51 +0800

    Update View.php

--- app/Services/View.php
+++ app/Services/View.php
@@ -18,13 +18,17 @@ class View
             $theme=Config::get('theme');
         }
         
-        
+        $can_backtoadmin = 0;
+        if (Utils\Cookie::get('old_uid') && Utils\Cookie::get('old_email') && Utils\Cookie::get('old_key') && Utils\Cookie::get('old_ip') && Utils\Cookie::get('old_expire_in') && Utils\Cookie::get('old_local')) {
+            $can_backtoadmin = 1;
+        }
         $smarty->settemplatedir(BASE_PATH.'/resources/views/'.$theme.'/'); //设置模板文件存放目录
         $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
         $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
         // add config
         $smarty->assign('config', Config::getPublicConfig());
         $smarty->assign('user', Auth::getUser());
+        $smarty->assign('can_backtoadmin', $can_backtoadmin);
         return $smarty;
     }
 }
