7bcdf30b3d2e3f627cab13a8d70dd4136c0f43a7
Indexyz <jiduye@gmail.com>
2018-08-09 00:38:09 +0800

    debug

--- app/Utils/URL.php
+++ app/Utils/URL.php
@@ -286,7 +286,7 @@ class URL
         $result = "";
 
         foreach ($nodes as $node) {
-            $result += URL::getV2Url($user, $node) . ' ';
+            $result += (URL::getV2Url($user, $node) . ' ');
         }
 
         return $result;
