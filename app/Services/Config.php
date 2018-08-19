86e1afdd9df464716770c4e4032cf01cf87c48b0
Soul Dark <1012179378@qq.com>
2018-08-19 23:34:03 +0800

    修复兼容单多BUG

--- app/Services/Config.php
+++ app/Services/Config.php
@@ -22,6 +22,7 @@ class Config
             "appName" => self::get("appName"),
             "version" => VERSION,
             "baseUrl" => self::get("baseUrl"),
+			"enable_old_mode"=>self::get("enable_old_mode"),
 			"min_port" => self::get("min_port"),
 			"max_port" => self::get("max_port"),
             "checkinMin" => self::get("checkinMin"),
