1c068848bf3f6c6ec1b94ae04102783e9ec80886
Tony Zou <tony200303.zt@gmail.com>
2018-09-11 17:40:03 +0800

    Cloudflare支持，添加节点时自动更新记录

---
+++ app/Utils/CloudflareDriver.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tonyzou
+ * Date: 2018/9/10
+ * Time: 上午9:54
+ */
+
+namespace App\Utils;
+use App\Services\Config;
+
+class CloudflareDriver
+{
+
+    // @todo: parameters
+    public static function modifyRecord(\Cloudflare\API\Endpoints\DNS $dns, $zoneID, $recordID, $name, $content, $proxied = false)
+    {
+        $details = ['type' => 'A', 'name' => $name, 'content' => $content, 'proxied' => $proxied];
+        if ($dns->updateRecordDetails($zoneID, $recordID, $details)->success == true) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static function addRecord(\Cloudflare\API\Endpoints\DNS $dns, $zoneID, $type, $name, $content, $ttl = 120, $proxied = false)
+    {
+        if ($dns->addRecord($zoneID, $type, $name, $content, $ttl, $proxied) == true) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static function updateRecord($name, $content, $proxied = false)
+    {
+        $key = new \Cloudflare\API\Auth\APIKey(Config::get('cloudflare_email'), Config::get('cloudflare_key'));
+        $adapter = new \Cloudflare\API\Adapter\Guzzle($key);
+        $zones = new \Cloudflare\API\Endpoints\Zones($adapter);
+
+        $zoneID = $zones->getZoneID(Config::get('cloudflare_name'));
+
+        $dns = new \Cloudflare\API\Endpoints\DNS($adapter);
+
+        $r = $dns->listRecords($zoneID, '', $name);
+        $recordCount = $r->result_info->count;
+        $records = $r->result;
+
+        if ($recordCount == 0){
+            self::addRecord($dns, $zoneID, 'A', $name, $content);
+        } elseif ($recordCount >= 1){
+            foreach ($records as $record){
+                $recordID = $record->id;
+                self::modifyRecord($dns, $zoneID, $recordID, $name, $content, $proxied);
+            }
+        }
+    }
+}
