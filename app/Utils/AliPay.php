69c5d362f6cebe1d32c29cd898c8b4f023d0c8af
chen <569922982@qq.com>
2018-09-14 11:16:56 +0800

    bug2

--- app/Utils/AliPay.php
+++ app/Utils/AliPay.php
@@ -245,13 +245,13 @@ class AliPay
         if ($this->getConfig('AliPay_Status') == 1 && $type == 1) {
             $name = '支付宝';
             self::setConfig('AliPay_Status', 0);
-            Mail::getClient()->send(Config::get('AliPay_EMail'), 'LOG报告监听' . $name . 'COOKIE出现问题',
+            Mail::getClient()->send($this->getConfig('Notice_EMail'), 'LOG报告监听' . $name . 'COOKIE出现问题',
                 "LOG提醒你，{$name}COOKIE出现问题，请务必尽快更新COOKIE。<br>LOG记录时间：$time", []);
         }
         if ($this->getConfig('WxPay_Status') == 1 && $type == 2) {
             $name = '微信';
             self::setConfig('WxPay_Status', 0);
-            Mail::getClient()->send(Config::get('AliPay_EMail'), 'LOG报告监听' . $name . 'COOKIE出现问题',
+            Mail::getClient()->send($this->getConfig('Notice_EMail'), 'LOG报告监听' . $name . 'COOKIE出现问题',
                 "LOG提醒你，{$name}COOKIE出现问题，请务必尽快更新COOKIE。<br>LOG记录时间：$time", []);
         }
     }
@@ -262,13 +262,13 @@ class AliPay
         if ($this->getConfig('AliPay_Status') == 0 && $type == 1) {
             $name = '支付宝';
             self::setConfig('AliPay_Status', 1);
-            Mail::getClient()->send(Config::get('AliPay_EMail'), 'LOG报告监听' . $name . 'COOKIE成功运行',
+            Mail::getClient()->send($this->getConfig('Notice_EMail'), 'LOG报告监听' . $name . 'COOKIE成功运行',
                 "LOG提醒你，{$name}COOKIE成功运行。<br>LOG记录时间：$time", []);
         }
         if ($this->getConfig('WxPay_Status') == 0 && $type == 2) {
             $name = '微信';
             self::setConfig('WxPay_Status', 1);
-            Mail::getClient()->send(Config::get('AliPay_EMail'), 'LOG报告监听' . $name . 'COOKIE成功运行',
+            Mail::getClient()->send($this->getConfig('Notice_EMail'), 'LOG报告监听' . $name . 'COOKIE成功运行',
                 "LOG提醒你，{$name}COOKIE成功运行。<br>LOG记录时间：$time", []);
         }
     }
