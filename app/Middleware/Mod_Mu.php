2e877c4a9eda673d47ab8843e8e4f0b9fda939f9
NimaQu <i@nimaqu.com>
2018-11-06 22:58:29 +0800

    在 webapi ip 鉴权失败的时候 json 带当前 IP

--- app/Middleware/Mod_Mu.php
+++ app/Middleware/Mod_Mu.php
@@ -41,7 +41,7 @@ class Mod_Mu
         $node = Node::where("node_ip", "LIKE", $_SERVER["REMOTE_ADDR"].'%')->first();
         if ($node==null && $_SERVER["REMOTE_ADDR"] != '127.0.0.1') {
             $res['ret'] = 0;
-            $res['data'] = "token or source is invalid";
+            $res['data'] = "token or source is invalid, Your ip address is ". $_SERVER["REMOTE_ADDR"];
             $response->getBody()->write(json_encode($res));
             return $response;
         }
