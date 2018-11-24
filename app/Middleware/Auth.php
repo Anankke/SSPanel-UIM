b3495125f5220874c35a57b2d0dead1439b3c43f
围墙 <admin@biye.la>
2018-11-24 20:49:38 +0800

    水昨天没改完的

--- app/Middleware/Auth.php
+++ app/Middleware/Auth.php
@@ -18,28 +18,10 @@ class Auth
             $newResponse = $response->withStatus(302)->withHeader('Location', '/auth/login');
             return $newResponse;
         }
-        
-        
         if ($user->enable == 0 && $_SERVER["REQUEST_URI"] != "/user/disable") {
             $newResponse = $response->withStatus(302)->withHeader('Location', '/user/disable');
             return $newResponse;
         }
-        
-        if (Config::get('enable_duoshuo')=='true') {
-            $token = array(
-                "short_name"=>Config::get('duoshuo_shortname'),
-                "user_key"=>$user->id,
-                "name"=>$user->user_name,
-                "email"=>$user->email
-            );
-            
-            
-            
-            $duoshuoToken = JWT::encode_withkey($token, Config::get('duoshuo_apptoken'));
-            
-            setcookie('duoshuo_token', $duoshuoToken);
-        }
-        
         $response = $next($request, $response);
         return $response;
     }
