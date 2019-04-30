06b5329f386584dcef5a77a8325abc64634e88ca
laurieryayoi <14360417+laurieryayoi@users.noreply.github.com>
2019-05-01 00:43:49 +0800

    修复firefox下剪贴板操作无效

--- uim-index-dev/src/directives/uimclip.js
+++ uim-index-dev/src/directives/uimclip.js
@@ -1,21 +1,16 @@
 export default {
   inserted: function (el, binding) {
     el.addEventListener('click', (e) => {
-      let copy = new Promise((resolve, reject) => {
-        let input = document.createElement('input')
-        let value = el.dataset.uimclip
-        input.setAttribute('type', 'text')
-        input.setAttribute('value', value)
-        el.after(input)
-        input.focus()
-        input.setSelectionRange(0, value.length)
-        document.execCommand('copy', true)
-        resolve(input)
-      })
-      copy.then((r) => {
-        r.remove()
-        binding.value.onSuccess()
-      })
+      let input = document.createElement('input')
+      let value = el.dataset.uimclip
+      input.setAttribute('type', 'text')
+      input.setAttribute('value', value)
+      el.after(input)
+      input.focus()
+      input.setSelectionRange(0, value.length)
+      document.execCommand('copy')
+      input.remove();
+      binding.value.onSuccess();
     })
   }
 }
