111c8eaf743cd3de73259d7a6e3bfcb04bb6c362
Anankke <anankke@protonmail.com>
2019-05-03 04:48:26 -0400

    修缩进

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -43,35 +43,35 @@ class XCat
             case("setTelegram"):
                 return $this->setTelegram();
             case("initQQWry"):
-                 return $this->initQQWry();
+                return $this->initQQWry();
             case("sendDiaryMail"):
                 return DailyMail::sendDailyMail();
-			case("sendFinanceMail_day"):
-			    return FinanceMail::sendFinanceMail_day();
-			case("sendFinanceMail_week"):
-			    return FinanceMail::sendFinanceMail_week();
-			case("sendFinanceMail_month"):
-			    return FinanceMail::sendFinanceMail_month();
+            case("sendFinanceMail_day"):
+                return FinanceMail::sendFinanceMail_day();
+            case("sendFinanceMail_week"):
+                return FinanceMail::sendFinanceMail_week();
+            case("sendFinanceMail_month"):
+                return FinanceMail::sendFinanceMail_month();
             case("reall"):
-                    return DailyMail::reall();
+                return DailyMail::reall();
             case("syncusers"):
-                    return SyncRadius::syncusers();
+                return SyncRadius::syncusers();
             case("synclogin"):
-                    return SyncRadius::synclogin();
+                return SyncRadius::synclogin();
             case("syncvpn"):
                 return SyncRadius::syncvpn();
             case("nousers"):
-                    return ExtMail::sendNoMail();
+                return ExtMail::sendNoMail();
             case("oldusers"):
-                    return ExtMail::sendOldMail();
+                return ExtMail::sendOldMail();
             case("syncnode"):
-                    return Job::syncnode();
+                return Job::syncnode();
             case("syncnasnode"):
-                    return Job::syncnasnode();
-			case("detectGFW"):
-				return Job::detectGFW();
+                return Job::syncnasnode();
+            case("detectGFW"):
+                return Job::detectGFW();
             case("syncnas"):
-                    return SyncRadius::syncnas();
+                return SyncRadius::syncnas();
             case("dailyjob"):
                 return Job::DailyJob();
             case("checkjob"):
@@ -80,8 +80,8 @@ class XCat
                 return Job::UserGa();
             case("backup"):
                 return Job::backup(false);
-			case("backupfull"):
-				return Job::backup(true);
+            case("backupfull"):
+                return Job::backup(true);
             case("initdownload"):
                 return $this->initdownload();
             case("updatedownload"):
@@ -90,39 +90,39 @@ class XCat
                 return $this->cleanRelayRule();
             case("resetPort"):
                 return $this->resetPort();
-	        case("resetAllPort"):
+            case("resetAllPort"):
                 return $this->resetAllPort();
-			case("update"):
-			    return Update::update($this);
+            case("update"):
+                return Update::update($this);
             case ("sendDailyUsageByTG"):
                 return $this->sendDailyUsageByTG();
-			case('npmbuild'):
-				return $this->npmbuild();
-			default:
+            case('npmbuild'):
+                return $this->npmbuild();
+            default:
                 return $this->defaultAction();
         }
     }
 
     public function defaultAction()
     {
-        echo(PHP_EOL."用法： php xcat [选项]".PHP_EOL);
-		echo("常用选项:".PHP_EOL);
-		echo("  createAdmin - 创建管理员帐号".PHP_EOL);
-		echo("  setDiscord - 设置 Discord 机器人".PHP_EOL);
-		echo("  setTelegram - 设置 Telegram 机器人".PHP_EOL);
-		echo("  cleanRelayRule - 清除所有中转规则".PHP_EOL);
-		echo("  resetPort - 重置单个用户端口".PHP_EOL);
-		echo("  resetAllPort - 重置所有用户端口".PHP_EOL);
-		echo("  initdownload - 下载 SSR 程序至服务器".PHP_EOL);
-		echo("  initQQWry - 下载 IP 解析库".PHP_EOL);
-		echo("  resetTraffic - 重置所有用户流量".PHP_EOL);
-		echo("  update - 更新并迁移配置".PHP_EOL);
+        echo(PHP_EOL . "用法： php xcat [选项]" . PHP_EOL);
+        echo("常用选项:" . PHP_EOL);
+        echo("  createAdmin - 创建管理员帐号" . PHP_EOL);
+        echo("  setDiscord - 设置 Discord 机器人" . PHP_EOL);
+        echo("  setTelegram - 设置 Telegram 机器人" . PHP_EOL);
+        echo("  cleanRelayRule - 清除所有中转规则" . PHP_EOL);
+        echo("  resetPort - 重置单个用户端口" . PHP_EOL);
+        echo("  resetAllPort - 重置所有用户端口" . PHP_EOL);
+        echo("  initdownload - 下载 SSR 程序至服务器" . PHP_EOL);
+        echo("  initQQWry - 下载 IP 解析库" . PHP_EOL);
+        echo("  resetTraffic - 重置所有用户流量" . PHP_EOL);
+        echo("  update - 更新并迁移配置" . PHP_EOL);
     }
 
-	public function resetPort()
+    public function resetPort()
     {
-		fwrite(STDOUT, "请输入用户id: ");
-        $user=User::Where("id", "=", trim(fgets(STDIN)))->first();
+        fwrite(STDOUT, "请输入用户id: ");
+        $user = User::Where("id", "=", trim(fgets(STDIN)))->first();
         $origin_port = $user->port;
 
         $user->port = Tools::getAvPort();
@@ -133,9 +133,9 @@ class XCat
             $rule->save();
         }
 
-		if ($user->save()) {
+        if ($user->save()) {
             echo "重置成功!\n";
-		}
+        }
     }
 
     public function resetAllPort()
@@ -144,7 +144,7 @@ class XCat
         foreach ($users as $user) {
             $origin_port = $user->port;
             $user->port = Tools::getAvPort();
-            echo '$origin_port='.$origin_port.'&$user->port='.$user->port."\n";
+            echo '$origin_port=' . $origin_port . '&$user->port=' . $user->port . "\n";
             $user->save();
         }
     }
@@ -153,9 +153,9 @@ class XCat
     {
         $rules = Relay::all();
         foreach ($rules as $rule) {
-            echo($rule->id."\n");
+            echo($rule->id . "\n");
             if ($rule->source_node_id == 0) {
-                echo($rule->id."被删除！\n");
+                echo($rule->id . "被删除！\n");
                 $rule->delete();
                 continue;
             }
@@ -163,7 +163,7 @@ class XCat
             $ruleset = Relay::where('user_id', $rule->user_id)->orwhere('user_id', 0)->get();
             $maybe_rule_id = Tools::has_conflict_rule($rule, $ruleset, $rule->id);
             if ($maybe_rule_id != 0) {
-                echo($rule->id."被删除！\n");
+                echo($rule->id . "被删除！\n");
                 $rule->delete();
             }
         }
@@ -171,7 +171,7 @@ class XCat
 
     public function initdownload()
     {
-        system('git clone https://github.com/xcxnig/ssr-download.git '.BASE_PATH."/public/ssr-download/", $ret);
+        system('git clone https://github.com/xcxnig/ssr-download.git ' . BASE_PATH . "/public/ssr-download/", $ret);
         echo $ret;
     }
 
@@ -197,7 +197,7 @@ class XCat
             $user->email = $email;
             $user->pass = Hash::passwordHash($passwd);
             $user->passwd = Tools::genRandomChar(6);
-            $user->port = Tools::getLastPort()+1;
+            $user->port = Tools::getLastPort() + 1;
             $user->t = 0;
             $user->u = 0;
             $user->d = 0;
@@ -205,20 +205,20 @@ class XCat
             $user->invite_num = Config::get('inviteNum');
             $user->ref_by = 0;
             $user->is_admin = 1;
-            $user->expire_in=date("Y-m-d H:i:s", time()+Config::get('user_expire_in_default')*86400);
-            $user->reg_date=date("Y-m-d H:i:s");
-            $user->money=0;
-            $user->im_type=1;
-            $user->im_value="";
-            $user->class=0;
-            $user->plan='A';
-            $user->node_speedlimit=0;
-            $user->theme=Config::get('theme');
+            $user->expire_in = date("Y-m-d H:i:s", time() + Config::get('user_expire_in_default') * 86400);
+            $user->reg_date = date("Y-m-d H:i:s");
+            $user->money = 0;
+            $user->im_type = 1;
+            $user->im_value = "";
+            $user->class = 0;
+            $user->plan = 'A';
+            $user->node_speedlimit = 0;
+            $user->theme = Config::get('theme');
 
             $ga = new GA();
             $secret = $ga->createSecret();
-            $user->ga_token=$secret;
-            $user->ga_enable=0;
+            $user->ga_token = $secret;
+            $user->ga_enable = 0;
 
             if ($user->save()) {
                 echo "Successful/添加成功!\n";
@@ -235,9 +235,9 @@ class XCat
     {
         try {
             User::where("enable", 1)->update([
-            'd' => 0,
-            'u' => 0,
-            'last_day_t' => 0,
+                'd' => 0,
+                'u' => 0,
+                'last_day_t' => 0,
             ]);
         } catch (\Exception $e) {
             echo $e->getMessage();
@@ -249,8 +249,8 @@ class XCat
     public function setTelegram()
     {
         $bot = new \TelegramBot\Api\BotApi(Config::get('telegram_token'));
-        if ($bot->setWebhook(Config::get('baseUrl')."/telegram_callback?token=".Config::get('telegram_request_token')) == 1) {
-            echo("设置成功！".PHP_EOL);
+        if ($bot->setWebhook(Config::get('baseUrl') . "/telegram_callback?token=" . Config::get('telegram_request_token')) == 1) {
+            echo("设置成功！" . PHP_EOL);
         }
     }
 
@@ -259,7 +259,7 @@ class XCat
         echo("downloading....");
         $qqwry = file_get_contents("https://qqwry.mirror.noc.one/QQWry.Dat");
         if ($qqwry != "") {
-            $fp = fopen(BASE_PATH."/storage/qqwry.dat", "wb");
+            $fp = fopen(BASE_PATH . "/storage/qqwry.dat", "wb");
             if ($fp) {
                 fwrite($fp, $qqwry);
                 fclose($fp);
@@ -267,29 +267,31 @@ class XCat
             echo("finish....");
         }
     }
+
     public function sendDailyUsageByTG()
     {
         $bot = new \TelegramBot\Api\BotApi(Config::get('telegram_token'));
-        $users = User::where('telegram_id',">",0)->get();
-        foreach ($users as $user){
-            $reply_message ="您当前的流量状况：
+        $users = User::where('telegram_id', ">", 0)->get();
+        foreach ($users as $user) {
+            $reply_message = "您当前的流量状况：
 今日已使用 " . $user->TodayusedTraffic() . " " . number_format(($user->u + $user->d - $user->last_day_t) / $user->transfer_enable * 100, 2) . "%
 今日之前已使用 " . $user->LastusedTraffic() . " " . number_format($user->last_day_t / $user->transfer_enable * 100, 2) . "%
 未使用 " . $user->unusedTraffic() . " " . number_format(($user->transfer_enable - ($user->u + $user->d)) / $user->transfer_enable * 100, 2) . "%
 					                        ";
-            try{
-                $bot->sendMessage($user->get_user_attributes("telegram_id"), $reply_message , $parseMode = null, $disablePreview = false, $replyToMessageId = null);
+            try {
+                $bot->sendMessage($user->get_user_attributes("telegram_id"), $reply_message, $parseMode = null, $disablePreview = false, $replyToMessageId = null);
 
-            } catch (\TelegramBot\Api\HttpException $e){
-                echo 'Message: 用户: '.$user->get_user_attributes("user_name")." 删除了账号或者屏蔽了宝宝";
+            } catch (\TelegramBot\Api\HttpException $e) {
+                echo 'Message: 用户: ' . $user->get_user_attributes("user_name") . " 删除了账号或者屏蔽了宝宝";
             }
         }
     }
 
-	public function npmbuild(){
-		chdir(BASE_PATH.'/uim-index-dev');
-		system('npm install');
-		system('npm run build');
-		system('cp -u ../public/vuedist/index.html ../resources/views/material/index.tpl');
-	}
+    public function npmbuild()
+    {
+        chdir(BASE_PATH . '/uim-index-dev');
+        system('npm install');
+        system('npm run build');
+        system('cp -u ../public/vuedist/index.html ../resources/views/material/index.tpl');
+    }
 }
