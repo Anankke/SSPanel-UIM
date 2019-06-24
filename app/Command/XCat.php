f6805b514e8c3a62c0ae8f6c45a84390363d5fbb
Anankke <anankke@protonmail.com>
2019-06-24 20:33:14 +0800

    perf: telegram daily usage output
    note to Anankke: 使用 git blame 锁定击杀目标

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -282,16 +282,26 @@ class XCat
         $bot = new BotApi(Config::get('telegram_token'));
         $users = User::where('telegram_id', '>', 0)->get();
         foreach ($users as $user) {
-            $reply_message = '您当前的流量状况： 今日已使用 ' . $user->TodayusedTraffic() . ' ' . number_format(
-                ($user->u + $user->d - $user->last_day_t) / $user->transfer_enable * 100,
-                2
-            ) . '% 今日之前已使用 ' . $user->LastusedTraffic() . ' ' . number_format(
-                $user->last_day_t / $user->transfer_enable * 100,
-                2
-            ) . '% 未使用 ' . $user->unusedTraffic() . ' ' . number_format(
-                ($user->transfer_enable - ($user->u + $user->d)) / $user->transfer_enable * 100,
-                2
-            ) . '% ';
+            $u = $user->u;
+            $d = $user->d;
+            $last_day_t = $user->last_day_t;
+            $transfer_enable = $user->transfer_enable;
+            $reply_message = '您当前的流量状况：' . PHP_EOL .
+                sprintf(
+                    '今天已使用 %s %s%%',
+                    $user->TodayusedTraffic(),
+                    number_format(($u + $d - $last_day_t) / $transfer_enable * 100, 2)
+                ) . PHP_EOL .
+                sprintf(
+                    '今天前已使用 %s %s%%',
+                    $user->LastusedTraffic(),
+                    number_format($last_day_t / $transfer_enable * 100, 2)
+                ) . PHP_EOL .
+                sprintf(
+                    '剩余 %s %s%%',
+                    $user->unusedTraffic(),
+                    number_format(($transfer_enable - ($u + $d)) / $transfer_enable * 100, 2)
+                );
             $bot->sendMessage(
                 $user->get_user_attributes('telegram_id'),
                 $reply_message,
