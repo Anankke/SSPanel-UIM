043a31d976fbd4217cdae7fee0bed61e244a43a9
SukkaW <isukkaw@gmail.com>
2019-07-07 22:23:51 +0800

    fix/ci: chmod permission & result output

--- test/test.sh
+++ test/test.sh
@@ -5,5 +5,6 @@ do
     http_code=$(curl -o /dev/null -s -m 10 --connect-timeout 5 -w %{http_code} "http://sspanel.test:23480$ROUTE")
     echo '--------------------------------------------------'
     echo "$ROUTE - http code: $http_code"
-    echo '--------------------------------------------------'
 done < ./test/route.list
+
+echo '--------------------------------------------------'
