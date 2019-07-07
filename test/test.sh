93a4dffc3478d5b2a9d5f8bc34beb8da8f6adfd6
SukkaW <isukkaw@gmail.com>
2019-07-07 22:07:21 +0800

    ci: add route tesh.sh

---
+++ test/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while read ROUTE
+do
+    http_code=$(curl -o /dev/null -s -m 10 --connect-timeout 5 -w %{http_code} "http://sspanel.test:23480$ROUTE")
+    echo '--------------------------------------------------'
+    echo "$ROUTE - http code: $http_code"
+    echo '--------------------------------------------------'
+done < ./test/route.list
