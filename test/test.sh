da3b8b596a6417f082b89d808ff6aa3a891c4ec4
SukkaW <isukkaw@gmail.com>
2019-07-08 02:15:49 +0800

    fix/ci: cookie arr & bring up cookie in curl

--- test/test.sh
+++ test/test.sh
@@ -2,10 +2,11 @@
 
 echo "key=$1"
 echo "expire_in=$2"
+cookie="uid=1; email=test@example.com; key=$1; expire_in=$2"
 
 while read ROUTE
 do
-    http_code=$(curl -o /dev/null -s -m 10 --connect-timeout 5 -w %{http_code} "http://sspanel.test:23480$ROUTE")
+    http_code=$(curl -o /dev/null -H"Cookie: $cookie" -s -m 10 --connect-timeout 5 -w %{http_code} "http://sspanel.test:23480$ROUTE")
     echo '--------------------------------------------------'
     echo "$ROUTE - http code: $http_code"
 done < ./test/route.list
