043131c3001a63a4cb347e5bc2367b53c4eab0ca
eeeeLeeee <me@odbook.com>
2019-08-01 18:47:31 +0800

    Update .config.example.php

--- config/.config.example.php
+++ config/.config.example.php
@@ -177,7 +177,7 @@ $System_Config['enable_checkin_captcha'] = 'false';	//启用签到验证码
 
 
 //支付系统设置----------------------------------------------------------------------------------------
-#取值 none | codepay | trimepay | f2fpay | chenAlipay | paymentwall | spay |tomatopay | payjs
+#取值 none | codepay | trimepay | f2fpay | chenAlipay | paymentwall | spay |bitpay | payjs
 $System_Config['payment_system']='none';
 
 #codepay码支付
