797bd35f1be17bb96f05b3b5eefffbb377958499
Anankke <anankke@protonmail.com>
2019-09-23 11:20:11 -0400

    revert: "fix(init): remove default http forward header handle"
    This reverts commit 040e987a

--- config/.config.example.php
+++ config/.config.example.php
@@ -319,6 +319,12 @@ $_ENV['relay_insecure_mode']='false';		//强烈推荐不开启
 $_ENV['enable_analytics_code']='false';
 $_ENV['sspanelAnalysis'] = 'true';
 
+#在套了CDN之后获取用户真实ip，如果您不知道这是什么，请不要乱动
+if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+$list = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+$_SERVER['REMOTE_ADDR'] = $list[0];
+}
+
 
 // make replace _ENV with env
 function findKeyName($name) {
