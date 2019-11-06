d06ebb83c420b14d2b978e239f0296bfddd9a635
SakuraSa233 <sakura.sa233@gmail.com>
2019-11-06 16:38:15 +0800

    refactor(middleware): style refactor and optimize

--- app/Middleware/Mod_Mu.php
+++ app/Middleware/Mod_Mu.php
@@ -1,24 +1,26 @@
 <?php
 
-
 namespace App\Middleware;
 
 use App\Services\Config;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use App\Utils\Helper;
 use App\Models\Node;
 
 class Mod_Mu
 {
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+    /**
+     * @param \Slim\Http\Request    $request
+     * @param \Slim\Http\Response   $response
+     * @param array                 $args
+     *
+     * @return \Slim\Http\Response
+     */
+    public function __invoke($request, $response, $next)
     {
-        $key = Helper::getParam($request, 'key');
+        $key = $request->getQueryParam('key');
         if ($key === null) {
             $res['ret'] = 0;
             $res['data'] = 'null';
-            $response->getBody()->write(json_encode($res));
-            return $response;
+            return $response->write(json_encode($res));
         }
 
         $auth = false;
@@ -29,23 +31,19 @@ class Mod_Mu
                 break;
             }
         }
-
         $node = Node::where('node_ip', 'LIKE', $_SERVER['REMOTE_ADDR'] . '%')->first();
         if ($node == null && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
             $res['ret'] = 0;
             $res['data'] = 'token or source is invalid, Your ip address is ' . $_SERVER['REMOTE_ADDR'];
-            $response->getBody()->write(json_encode($res));
-            return $response;
+            return $response->write(json_encode($res));
         }
 
         if ($auth == false) {
             $res['ret'] = 0;
             $res['data'] = 'token or source is invalid';
-            $response->getBody()->write(json_encode($res));
-            return $response;
+            return $response->write(json_encode($res));
         }
 
-        $response = $next($request, $response);
-        return $response;
+        return $next($request, $response);
     }
 }
