ed11b5e63b048ab95405ce29c2b56c5e2463a986
Incisakura <Incisakura@gmail.com>
2020-01-08 20:09:45 +0900

    feat(model): useful method and property doc

--- src/Models/Ann.php
+++ src/Models/Ann.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 /**
  * Ann Model
+ *
+ * @property-read   int    $id         Announcement ID
+ * @property        string $date       Date announcement posted
+ * @property        string $content    Announcement in HTML
+ * @property        string $markdown   Announcement in MarkDown
  */
 class Ann extends Model
 {
