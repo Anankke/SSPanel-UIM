1e2b63e59cb9b67b58c6f5a6968b8bb319699f17
ainuoyan <ainuoyan@users.noreply.github.com>
2020-05-10 05:32:29 +0800

    订阅链接兼容现有trojan客户端 (#1038)

--- src/Utils/AppURI.php
+++ src/Utils/AppURI.php
@@ -584,7 +584,7 @@ class AppURI
         switch ($item['type']) {
             case 'trojan':
                 $return  = ('trojan://' . $item['passwd'] . '@' . $item['address'] . ':' . $item['port']);
-                $return .= ('?peer=' . $item['host'] . '#' .  rawurlencode($item['remark']));
+                $return .= ('?peer=' . $item['host'] . '&sni=' . $item['host'] . '#' .  rawurlencode($item['remark']));
                 break;
         }
         return $return;
