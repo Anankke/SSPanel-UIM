b3ca72729c22c4acee06acd91ae34380047449ea
Incisakura <incisakura@gmail.com>
2020-08-07 13:40:02 +0800

    fix: lint

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php
 
-declare(strict_types=1);
 /**
  * SSPanel-Uim Public Entrance File
  *
  * @license MIT(https://github.com/Anankke/SSPanel-Uim/blob/dev/LICENSE)
  *          Addition: You shouldn't remove staff page or entrance of that page.
  */
+
+declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/.config.php';
