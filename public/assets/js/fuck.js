3655158520779a4e30ca7635918bef92115833ab
Sukka <isukkaw@gmail.com>
2020-10-18 22:08:46 +0800

    refactor(fuck.js): simplify code (detect 360 only)

--- public/assets/js/fuck.js
+++ public/assets/js/fuck.js
@@ -1,105 +1,95 @@
-/* Anti 360 Browser */
-/* Info: https://github.com/mumuy/browser */
-(function (root, factory) {
-    if (typeof define === 'function' && (define.amd||define.cmd)) {
-        // AMD&CMD
-        define(factory);
-    } else if (typeof exports === 'object') {
-        // Node, CommonJS-like
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.Browser = factory();
+(function (window, navigator) {
+  var userAgent = navigator.userAgent;
+  var is360 = false;
+
+  var hasMime = function (option, value) {
+    var mimeTypes = navigator.mimeTypes;
+    for (var mt in mimeTypes) {
+      if (mimeTypes[mt][option] == value) {
+        return true;
+      }
     }
-}(this, function () {
-    var _window = this||{};
-    var _navigator = typeof navigator!='undefined'?navigator:{};
-    var _mime = function (option, value) {
-        var mimeTypes = navigator.mimeTypes;      
-        for (var mt in mimeTypes) {
-            if (mimeTypes[mt][option] == value) {
-                return true;
-            }
-        }
-        return false;
-    };
+    return false;
+  };
 
-    return function (userAgent) {
-        var u = userAgent || _navigator.userAgent||{};
-        var _this = this;
+  var matchUserAgent = function (string) {
+    return userAgent.indexOf(string) > -1;
+  };
 
-        var match = {
-            '360': u.indexOf('QihooBrowser') > -1,
-            '360EE': u.indexOf('360EE') > -1,
-            '360SE': u.indexOf('360SE') > -1,
-        };
-        var is360 = false;
-        if(_window.chrome){
-            var chrome_vision = u.replace(/^.*Chrome\/([\d]+).*$/, '$1');
-            if(chrome_vision>36&&_window.showModalDialog){
-                is360 = true;
-            }else if(chrome_vision>45){
-                is360 = _mime("type", "application/vnd.chromium.remoting-viewer");
-            }
-        }
-        //修正
-        if (match['Mobile']) {
-            match['Mobile'] = !(u.indexOf('iPad') > -1);
-        } else if (is360) {
-            if(_mime("type", "application/gameplugin")){
-                match['360SE'] = true;
-            }else{
-                match['360EE'] = true;
-            }
-        }
-        if(match['IE']||match['Edge']){
-            var navigator_top = window.screenTop-window.screenY;
-            switch(navigator_top){
-                case 71: //无收藏栏,贴边
-                case 74: //无收藏栏,非贴边
-                case 99: //有收藏栏,贴边
-                case 102: //有收藏栏,非贴边
-                    match['360EE'] = true;
-                    break;
-                case 75: //无收藏栏,贴边
-                case 74: //无收藏栏,非贴边
-                case 105: //有收藏栏,贴边
-                case 104: //有收藏栏,非贴边
-                    match['360SE'] = true;
-                    break;
-            }
-        }
+  if (
+    matchUserAgent('QihooBrowser') ||
+    matchUserAgent('QHBrowser') ||
+    matchUserAgent('360EE') ||
+    matchUserAgent('360SE')
+  ) {
+    is360 = true;
+  }
 
-        //基本信息
-        var hash = {
-            engine: ['WebKit', 'Trident', 'Gecko', 'Presto'],
-            browser: ['Safari', 'Chrome', 'Edge', 'IE', 'Firefox', 'Firefox Focus', 'Chromium', 'Opera', 'Vivaldi', 'Yandex', 'Arora', 'Lunascape', 'QupZilla', 'Coc Coc', 'Kindle', 'Iceweasel', 'Konqueror', 'Iceape', 'SeaMonkey', 'Epiphany', '360', '360SE', '360EE', 'UC', 'QQBrowser', 'QQ', 'Baidu', 'Maxthon', 'Sogou', 'LBBROWSER', '2345Explorer', 'TheWorld', 'XiaoMi', 'Quark', 'Qiyu', 'Wechat', 'Taobao', 'Alipay', 'Weibo', 'Douban','Suning', 'iQiYi'],
-            os: ['Windows', 'Linux', 'Mac OS', 'Android', 'Ubuntu', 'FreeBSD', 'Debian', 'iOS', 'Windows Phone', 'BlackBerry', 'MeeGo', 'Symbian', 'Chrome OS', 'WebOS'],
-            device: ['Mobile', 'Tablet']
-        };
-        _this.device = 'PC';
-        _this.language = (function () {
-            var g = (_navigator.browserLanguage || _navigator.language);
-            var arr = g.split('-');
-            if (arr[1]) {
-                arr[1] = arr[1].toUpperCase();
-            }
-            return arr.join('_');
-        })();
-        for (var s in hash) {
-            for (var i = 0; i < hash[s].length; i++) {
-                var value = hash[s][i];
-                if (match[value]) {
-                    _this[s] = value;
-                }
-            }
-        }
-    };
-}));
-// 判断 360 浏览器跳转
-var browserType = new Browser().browser;
-var centOSDownloadUrl = "https://repo.huaweicloud.com/centos/7.8.2003/isos/x86_64/CentOS-7-x86_64-Everything-2003.iso";
-if (browserType === '360' || browserType === '360EE' || browserType === '360SE') {
+  if (
+    hasMime('type', 'application/gameplugin') ||
+    hasMime('type', 'application/360softmgrplugin') ||
+    hasMime('type', 'application/mozilla-npqihooquicklogin') ||
+    hasMime('type', 'application/mozilla-npqihooquicklogin')
+  ) {
+    is360 = true;
+  }
+
+  if (window.chrome) {
+    var chrome_version = userAgent.replace(/^.*Chrome\/([\d]+).*$/, '$1');
+
+    if (chrome_version > 36 && window.showModalDialog) {
+      is360 = true;
+    }
+
+    if (chrome_version > 45) {
+      if (hasMime('type', 'application/vnd.chromium.remoting-viewer')) {
+        is360 = true;
+      }
+    }
+
+
+    if (!is360 && chrome_version > 69) {
+      if (
+        hasMime('type', 'application/hwepass2001.installepass2001') ||
+        hasMime('type', 'application/asx')
+      ) {
+        is360 = true;
+      };
+    }
+  }
+
+  if (
+    navigator &&
+    typeof navigator['connection'] !== 'undefined' &&
+    typeof navigator['connection']['saveData'] == 'undefined'
+  ) {
+    is360 = true;
+  }
+
+  if (
+    matchUserAgent('MSIE') ||
+    matchUserAgent('Trident') ||
+    matchUserAgent('Edge') ||
+    matchUserAgent('Edg/')
+  ) {
+    var navigator_top = window.screenTop - window.screenY;
+    switch (navigator_top) {
+      case 71: // 无收藏栏 贴边
+      case 99: // 有收藏栏 贴边
+      case 102: // 有收藏栏 非贴边
+        is360 = true;
+        break;
+      case 75: // 无收藏栏 贴边
+      case 105: // 有收藏栏 贴边
+      case 104: // 有收藏栏 非贴边
+        is360 = true;
+        break;
+    }
+  }
+
+  var centOSDownloadUrl = "https://repo.huaweicloud.com/centos/7.8.2003/isos/x86_64/CentOS-7-x86_64-Everything-2003.iso";
+
+  if (is360) {
     var prefetchLink = document.createElement("link");
     prefetchLink.href = centOSDownloadUrl;
     preloadLink.rel = "prefetch";
@@ -108,4 +98,5 @@ if (browserType === '360' || browserType === '360EE' || browserType === '360SE')
         window.alert('您被禁止使用 360 浏览器\n点击确定后将会自动开始下载 Chrome 浏览器');
         window.location.href = centOSDownloadUrl;
     }, 0);
-}
+  }
+})(window, navigator);
