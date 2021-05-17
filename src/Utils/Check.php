9e8550117c6130d0c76dd7662308e3a6dad9ada6
VeryAdd <1339109608@qq.com>
2021-05-18 04:01:32 +0800

    Update Check.php

--- src/Utils/Check.php
+++ src/Utils/Check.php
@@ -10,4 +10,14 @@ class Check
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($email) <= 32;
     }
+    
+    // 禁止 Gmail 邮箱带 + 号的小号注册
+    public static function isGmailSmall($email)
+    {
+        $email_exp = explode('@', $email);
+        if ( $email_exp[1] === 'gmail.com' && strstr($email_exp[0], '+') ) {
+            return false;
+        }
+        return true;
+    }
 }
