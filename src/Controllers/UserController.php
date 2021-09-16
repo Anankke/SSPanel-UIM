644f1c0a4c5554e4529c413723ea6b92e931fa12
iamsaltedfish <90859800+iamsaltedfish@users.noreply.github.com>
2021-09-17 00:56:28 +0800

    Update UserController.php
    https://github.com/Anankke/SSPanel-Uim/issues/1321

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -669,8 +669,10 @@ class UserController extends BaseController
         $user->pass = $hashPwd;
         $user->save();
 
-        $user->clean_link();
-
+        if ($_ENV['enable_forced_replacement'] == true) {
+            $user->clean_link();
+        }
+        
         $res['ret'] = 1;
         $res['msg'] = '修改成功';
         return $response->withJson($res);
