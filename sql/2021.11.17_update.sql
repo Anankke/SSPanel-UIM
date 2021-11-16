cbe394f216d7f842bd8ea7909f67ddc148eac6b6
iamsaltedfish <90859800+iamsaltedfish@users.noreply.github.com>
2021-11-17 00:07:44 +0800

    添加更新sql文件

---
+++ sql/2021.11.17_update.sql
@@ -0,0 +1,2 @@
+INSERT INTO `config` (`id`, `item`, `value`, `class`, `is_public`, `type`, `default`, `mark`) VALUES
+(120, 'rebate_time_range_limit', '180', 'invite', '0', 'int', '180', '返利时间范围限制（单位：天）');
