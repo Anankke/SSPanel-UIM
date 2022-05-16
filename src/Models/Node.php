888e2a28a7ef282a452ff64abfdc78589a254c10
Cat <14369594+M1Screw@users.noreply.github.com>
2022-05-16 09:28:57 +0800

    Update Node.php

--- src/Models/Node.php
+++ src/Models/Node.php
@@ -450,8 +450,8 @@ final class Node extends Model
         $item['remark'] = ($emoji ? Tools::addEmoji($this->name) : $this->name);
         $item['type'] = 'trojan';
         $item['address'] = $server[0];
-        $item['port'] = (isset($opt['offset_port_user']) ? (int) $opt['offset_port_user'] : (isset($opt['offset_port_node']) ? (int) $opt['offset_port_node'] : (int) substr(explode('|', $server[1])[0], 5, strpos($server[1], '#') ? strpos($server[1], '#') - 5 : 5)));
-        $item['passwd'] = $user->uuid;
+        $item['port'] = (!empty($opt['offset_port_user']) ? (int) $opt['offset_port_user'] : (!empty($opt['offset_port_node']) ? (int) $opt['offset_port_node'] : 443));
+        $item['passwd'] = !empty($user->uuid) ? $user->uuid : '';
         $item['host'] = $item['address'];
         $item['net'] = (isset($opt['grpc']) ? 'grpc' : (strpos(implode($server), 'grpc=1') ? 'grpc' : ''));
         $item['servicename'] = ($opt['servicename'] ?? (strpos(implode($server), 'servicename=') ? substr(explode('|', $server[1])[3], 12) : ''));
