c9b8d661823b216f716f3ea77353d3a2f43e9226
iamsaltedfish <90859800+iamsaltedfish@users.noreply.github.com>
2022-05-20 19:38:33 +0800

    issue #1473

--- src/Services/Gateway/Epay/EpayTool.php
+++ src/Services/Gateway/Epay/EpayTool.php
@@ -128,7 +128,7 @@ final class EpayTool
     {
         $fp = fopen('/storage/epaylog.txt', 'a');
         flock($fp, LOCK_EX);
-        fwrite($fp, '执行日期：'.strftime('%Y%m%d%H%M%S', time())."\n".$word."\n");
+        fwrite($fp, '执行日期：'.date('Y-m-d H:i:s', time())."\n".$word."\n");
         flock($fp, LOCK_UN);
         fclose($fp);
     }
