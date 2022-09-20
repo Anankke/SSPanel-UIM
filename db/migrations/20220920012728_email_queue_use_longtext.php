a002e92194dd20395b23661ec5c2a0ba256000c1
Cat <14369594+M1Screw@users.noreply.github.com>
2022-09-20 11:06:09 +0800

    fix: email_queue array type

---
+++ db/migrations/20220920012728_email_queue_use_longtext.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class EmailQueueUseLongtext extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('email_queue')
+            ->changeColumn('array', 'longtext')
+            ->save();
+    }
+
+    public function down(): void
+    {
+        $this->table('email_queue')
+            ->changeColumn('array', 'text')
+            ->save();
+    }
+}
