38d0724e6b6a2bbaf434d3664f4a1046c7ada7c3
Cat <14369594+M1Screw@users.noreply.github.com>
2022-10-17 02:47:49 +0800

    fix: remove mu_node & ndoe class in ServerController

--- src/Controllers/User/ServerController.php
+++ src/Controllers/User/ServerController.php
@@ -6,9 +6,7 @@ namespace App\Controllers\User;
 
 use App\Controllers\BaseController;
 use App\Models\Node;
-use App\Models\User;
 use App\Utils\Tools;
-use App\Utils\URL;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -33,8 +31,6 @@ final class ServerController extends BaseController
         $nodes = $query->orderBy('node_class')->orderBy('name')->get();
         $all_node = [];
         foreach ($nodes as $node) {
-            /** @var Node $node */
-
             $array_node = [];
             $array_node['id'] = $node->id;
             $array_node['name'] = $node->name;
@@ -52,30 +48,7 @@ final class ServerController extends BaseController
             $array_node['traffic_limit'] = (int) Tools::flowToGB($node->node_bandwidth_limit);
             $array_node['bandwidth'] = $node->getNodeSpeedlimit();
 
-            $all_connect = [];
-            if (\in_array($node->sort, [0])) {
-                if ($node->mu_only !== 1) {
-                    $all_connect[] = 0;
-                }
-                if ($node->mu_only !== -1) {
-                    $mu_node_query = Node::query();
-                    $mu_node_query->where('sort', 9)->where('type', '1');
-                    if (! $user->is_admin) {
-                        $mu_node_query->where('node_class', '<=', $user->class)->whereIn('node_group', $group);
-                    }
-                    $mu_nodes = $mu_node_query->get();
-                    foreach ($mu_nodes as $mu_node) {
-                        if (User::where('port', $mu_node->server)->where('is_multi_user', '<>', 0)->first() !== null) {
-                            $all_connect[] = $node->getOffsetPort($mu_node->server);
-                        }
-                    }
-                }
-            } else {
-                $all_connect[] = 0;
-            }
-            $array_node['connect'] = $all_connect;
-
-            $all_node[$node->node_class + 1000][] = $array_node;
+            $all_node[] = $array_node;
         }
 
         return $response->write(
