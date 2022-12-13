4a3959fb5aeee69797b706060eeb83c6c673eb1e
KorenKrita <KorenKrita@gmail.com>
2022-12-13 22:36:48 +0800

    change get node ip from ipv4 or ipv6

--- src/Models/Node.php
+++ src/Models/Node.php
@@ -160,16 +160,21 @@ final class Node extends Model
      */
     public function changeNodeIp(string $server_name): bool
     {
-        if (! Tools::isIPv4($server_name)) {
-            $ip = gethostbyname($server_name);
-            if ($ip === '') {
-                return false;
-            }
-        } else {
-            $ip = $server_name;
+        $dns = dns_get_record($server_name,DNS_A + DNS_AAAA)[0];
+        if(array_key_exists('ip', $dns)){
+            $ip = $dns['ip'];
+        }
+        else if (array_key_exists('ipv6', $dns)){
+            $ip = $dns['ipv6'];
+        }
+        else{
+            $ip = '';
         }
         $this->node_ip = $ip;
-        return true;
+        if ($ip === '') {
+            $ip = $server_name;
+        }
+                return true;
     }
 
     /**
