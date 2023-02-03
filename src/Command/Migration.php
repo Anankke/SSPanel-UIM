35e5df681b70e075123b1fea7607d8b318ed9db8
M1Screw <14369594+M1Screw@users.noreply.github.com>
2023-02-04 06:57:36 +0800

    feat: update.sh now support use release tag

--- src/Command/Migration.php
+++ src/Command/Migration.php
@@ -21,9 +21,9 @@ final class Migration extends Command
 {
     public $description = <<< END
 ├─=: php xcat Migration [版本]
-│ ├─ <version>               - 迁移至指定版本（前进/退回）
-│ ├─ latest                  - 迁移至最新版本
-│ ├─ new                     - 导入全新数据库至最新版本
+│├─ <version>               - 迁移至指定版本（前进/退回）
+│├─ latest                  - 迁移至最新版本
+│├─ new                     - 导入全新数据库至最新版本
 END;
 
     public function boot(): void
--- update.sh
+++ update.sh
@@ -1,18 +1,58 @@
 #!/usr/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+cat << "EOF"
+SSPanel-UIM update script
+Author: M1Screw
+Github: https://github.com/sspanel-uim/SSPanel-Uim-Dev
+Usage: 
+./update.sh dev --> Upgrade to the latest development version
+./update.sh release release_version db_version --> Upgrade to the specified release version
+EOF
 
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script!"; exit 1; }
 
-do_update_sspanel(){
+do_update_sspanel_dev(){
     git pull
     git reset --hard origin/dev
     git fetch --prune --prune-tags
     rm -r storage/framework/smarty/compile/*
     php composer.phar update
     php composer.phar selfupdate
-    php vendor/bin/phinx migrate
+    php xcat Migration latest
     php xcat Update
     php xcat Tool importAllSettings
     wget https://cdn.jsdelivr.net/gh/sspanel-uim/qqwry.dat@latest/qqwry.dat -O storage/qqwry.dat 
 }
 
-do_update_sspanel
+do_update_sspanel_release(){
+    tag=$1
+    db_version=$2
+    git pull
+    git reset --hard $tag
+    rm -r storage/framework/smarty/compile/*
+    php composer.phar update
+    php composer.phar selfupdate
+    php xcat Migration $db_version
+    php xcat Update
+    php xcat Tool importAllSettings
+    wget https://cdn.jsdelivr.net/gh/sspanel-uim/qqwry.dat@latest/qqwry.dat -O storage/qqwry.dat 
+}
+
+if [[ $1 == "dev" ]]; then
+    do_update_sspanel_dev
+    exit 0
+fi
+
+if [[ $1 == "release" ]]; then
+    if [[ $2 == "" ]]; then
+        echo "Error: The release version cannot be empty!"
+        exit 1
+    fi
+    if [[ $3 == "" ]]; then
+        echo "Error: The database version cannot be empty!"
+        exit 1
+    fi
+    do_update_sspanel_release $2 $3
+    exit 0
+fi
