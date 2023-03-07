711c5d8a210971d27bb6edfbe761fa4c0f7ecab0
M1Screw <14369594+M1Screw@users.noreply.github.com>
2023-03-07 19:51:22 +0800

    chore: code clean up for new oauth system

--- src/Utils/Telegram/Message.php
+++ src/Utils/Telegram/Message.php
@@ -57,51 +57,33 @@ final class Message
             $MessageData = trim($this->Message->getText());
             if ($this->ChatID > 0) {
                 // 私聊
-                if ($this->User !== null) {
-                    if (is_numeric($MessageData) && strlen((string) $MessageData) === 6) {
-                        $uid = TelegramSessionManager::verifyLoginNumber($MessageData, $this->User->id);
-                        if ($uid !== 0) {
-                            $text = '登录验证成功，邮箱：' . $this->User->email;
+                if (strlen($MessageData) === 16) {
+                    $Uid = TelegramSessionManager::verifyBindSession($MessageData);
+                    if ($Uid === 0) {
+                        $text = '绑定失败了呢，经检查发现：【' . $MessageData . '】的有效期为 10 分钟，您可以在我们网站上的 **资料编辑** 页面刷新后重试.';
+                    } else {
+                        $BinsUser = TelegramTools::getUser($Uid, 'id');
+                        $BinsUser->telegram_id = $this->triggerUser['id'];
+                        $BinsUser->im_type = 4;
+                        $BinsUser->im_value = $this->triggerUser['username'];
+                        $BinsUser->save();
+                        if ($BinsUser->is_admin >= 1) {
+                            $text = '尊敬的**管理员**您好，恭喜绑定成功。' . PHP_EOL . '当前绑定邮箱为：' . $BinsUser->email;
                         } else {
-                            $text = '登录验证失败，数字无效';
-                        }
-                        $bot->sendMessage(
-                            [
-                                'chat_id' => $this->ChatID,
-                                'text' => $text,
-                                'parse_mode' => 'Markdown',
-                            ]
-                        );
-                    }
-                } else {
-                    if (strlen($MessageData) === 16) {
-                        $Uid = TelegramSessionManager::verifyBindSession($MessageData);
-                        if ($Uid === 0) {
-                            $text = '绑定失败了呢，经检查发现：【' . $MessageData . '】的有效期为 10 分钟，您可以在我们网站上的 **资料编辑** 页面刷新后重试.';
-                        } else {
-                            $BinsUser = TelegramTools::getUser($Uid, 'id');
-                            $BinsUser->telegram_id = $this->triggerUser['id'];
-                            $BinsUser->im_type = 4;
-                            $BinsUser->im_value = $this->triggerUser['username'];
-                            $BinsUser->save();
-                            if ($BinsUser->is_admin >= 1) {
-                                $text = '尊敬的**管理员**您好，恭喜绑定成功。' . PHP_EOL . '当前绑定邮箱为：' . $BinsUser->email;
+                            if ($BinsUser->class >= 1) {
+                                $text = '尊敬的 **VIP ' . $BinsUser->class . '** 用户您好.' . PHP_EOL . '恭喜您绑定成功，当前绑定邮箱为：' . $BinsUser->email;
                             } else {
-                                if ($BinsUser->class >= 1) {
-                                    $text = '尊敬的 **VIP ' . $BinsUser->class . '** 用户您好.' . PHP_EOL . '恭喜您绑定成功，当前绑定邮箱为：' . $BinsUser->email;
-                                } else {
-                                    $text = '绑定成功了，您的邮箱为：' . $BinsUser->email;
-                                }
+                                $text = '绑定成功了，您的邮箱为：' . $BinsUser->email;
                             }
                         }
-                        $this->bot->sendMessage(
-                            [
-                                'chat_id' => $this->ChatID,
-                                'text' => $text,
-                                'parse_mode' => 'Markdown',
-                            ]
-                        );
                     }
+                    $this->bot->sendMessage(
+                        [
+                            'chat_id' => $this->ChatID,
+                            'text' => $text,
+                            'parse_mode' => 'Markdown',
+                        ]
+                    );
                 }
             }
             return;
