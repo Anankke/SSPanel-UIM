1d0df1d7e903a7cf931647f0dfe66e19a4c15f6e
M1Screw <14369594+M1Screw@users.noreply.github.com>
2023-03-12 21:14:11 +0800

    feat: redir user to the new shop

--- src/Controllers/Admin/ProductController.php
+++ src/Controllers/Admin/ProductController.php
@@ -80,8 +80,8 @@ final class ProductController extends BaseController
         $speed_limit = $request->getParam('speed_limit');
         $ip_limit = $request->getParam('ip_limit');
         // limit
-        $class_required = $request->getParam('class_required');
-        $node_group_required = $request->getParam('node_group_required');
+        $class_required = $request->getParam('class_required') ?? '';
+        $node_group_required = $request->getParam('node_group_required') ?? '';
         $new_user_required = $request->getParam('new_user_required') === 'true' ? 1 : 0;
 
         try {
@@ -203,8 +203,8 @@ final class ProductController extends BaseController
         $speed_limit = $request->getParam('speed_limit');
         $ip_limit = $request->getParam('ip_limit');
         // limit
-        $class_required = $request->getParam('class_required');
-        $node_group_required = $request->getParam('node_group_required');
+        $class_required = $request->getParam('class_required') ?? '';
+        $node_group_required = $request->getParam('node_group_required') ?? '';
         $new_user_required = $request->getParam('new_user_required') === 'true' ? 1 : 0;
 
         try {
