c2e7ac0d67121a1594413d154c1b26419e8669bf
M1Screw <14369594+M1Screw@users.noreply.github.com>
2023-04-04 23:06:12 +0800

    fix: broken install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,11 @@ do_install_sspanel() {
     echo "Generating config files..."
     cp config/.config.example.php config/.config.php
     cp config/appprofile.example.php config/appprofile.php
-    mv db/migrations/20000101000000_init_database.php.new db/migrations/20000101000000_init_database.php
     echo "Installing Composer..."
     wget https://getcomposer.org/installer -O composer.phar
     php composer.phar
     php composer.phar install
-    echo "Writting configuration..."
+    echo "Writing configuration..."
     sed -i -e "s/$_ENV['key']        = 'ChangeMe';/$_ENV['key']        = '$key';/g" \
     -e "s/$_ENV['appName']    = 'SSPanel-UIM';/$_ENV['appName']    = '$app_name';/g" \
     -e "s|$_ENV['baseUrl']    = 'https://example.com';|$_ENV['baseUrl']    = '$base_url';|g" \
@@ -32,7 +31,7 @@ do_install_sspanel() {
     -e "s/$_ENV['db_password']  = 'sspanel';/$_ENV['db_password']  = '$db_password';/g" \
     config/.config.php
     echo "Creating database and user..."
-    mysql -uroot -p$db_root_password \
+    mysql -uroot -p $db_root_password \
     -e "CREATE DATABASE $db_database CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
     CREATE USER '$db_username'@'localhost';
     GRANT ALL PRIVILEGES ON $db_database.* TO '$db_username'@'localhost' IDENTIFIED BY '$db_password';
@@ -40,9 +39,9 @@ do_install_sspanel() {
     echo "Importing config to database..."
     php xcat Migration new
     php xcat Tool importAllSettings
-    wget https://cdn.jsdelivr.net/gh/sspanel-uim/qqwry.dat@latest/qqwry.dat -O storage/qqwry.dat
     current_dir=$(pwd)
     crontab -l > cron.tmp
+    echo "*/5 * * * * /usr/bin/php $current_dir/xcat Cron" >> cron.tmp
     echo "*/1 * * * * /usr/bin/php $current_dir/xcat Job CheckJob" >> cron.tmp
     echo "0 */1 * * * /usr/bin/php $current_dir/xcat Job UserJob" >> cron.tmp
     echo "0 0 * * *   /usr/bin/php -n $current_dir/xcat Job DailyJob" >> cron.tmp
