ef4a501f4cfeafaf04263ee64592458edc6c1212
M1Screw <14369594+M1Screw@users.noreply.github.com>
2023-04-07 03:06:28 +0800

    fix: reset password token is not saved
    https://github.com/Anankke/SSPanel-Uim/issues/1896

--- src/Services/Password.php
+++ src/Services/Password.php
@@ -21,6 +21,8 @@ final class Password
         $pwdRst->init_time = time();
         $pwdRst->expire_time = time() + 3600 * 24;
         $pwdRst->token = Tools::genRandomChar(64);
+        $pwdRst->save();
+
         $subject = $_ENV['appName'] . '重置密码';
         $resetUrl = $_ENV['baseUrl'] . '/password/token/' . $pwdRst->token;
 
