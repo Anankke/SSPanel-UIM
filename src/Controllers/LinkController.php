1a6e3fde01acba79aae2e7b27ebdb94b5858a4f8
root <wyx2685@gmail.com>
2023-08-23 15:32:17 +0900

    Add vless subscribe link.

--- src/Controllers/LinkController.php
+++ src/Controllers/LinkController.php
@@ -190,7 +190,88 @@ final class LinkController extends BaseController
             } else {
                 $server = $node_custom_config['server_user'];
             }
-            if ((int) $node_raw->sort === 11) {
+            //跳过非v2ray型节点
+            if (! (int) $node_raw->sort === 11) {
+                continue;
+            }
+            //检查是否vless
+            $enable_vless = array_key_exists('enable_vless', $node_custom_config) ? $node_custom_config['enable_vless'] : '0';
+            if ((int) $enable_vless === 1) {
+                $config = [
+                    //vless基本字段
+                    "name" => self::encodeURIComponent($node_raw->name),
+                    "add" => $server,
+                    "port" => $node_custom_config['v2_port'] ?? ($node_custom_config['offset_port_user'] ?? ($node_custom_config['offset_port_node'] ?? 443)),
+                    "type" => $node_custom_config['network'] ?? 'tcp',
+                    "encryption" => 'none',
+                    //network区分字段
+                    "host" => $node_custom_config['host'] ?? '',
+                    "path" => self::encodeURIComponent($node_custom_config['path'] ?? '/'),
+                    "headerType" => 'none',
+                    "quicSecurity" => $node_custom_config['quicSecurity'] ?? 'none',
+                    "serviceName" => self::encodeURIComponent($node_custom_config['serviceName'] ?? ($node_custom_config['servicename'] ?? '')),
+                    "mode" => 'gun',
+                    //tls、xtls、reality支持
+                    "security" => $node_custom_config['security'] ?? 'none',
+                    "flow" => $node_custom_config['flow'] ?? 'none',
+                    "sni" => $node_custom_config['host'] ?? '',
+                    "pbk" => $node_custom_config['publicKey'] ?? '',
+                    "sid" => $node_custom_config['shortId'] ?? '',
+                ];
+                $output = "vless://" . $user->uuid . "@" . $config['add'] . ":" . $config['port'];
+                $output .= "?type={$config['type']}&encryption={$config['encryption']}&security={$config['security']}";
+
+                if ($config['security'] !== 'none') {
+                    $output .= "&sni={$config['sni']}";
+                    if ($config['flow'] !== 'none') {
+                        $output .= "&flow={$config['flow']}";
+                    }
+                    if ((string) $config['security'] === 'reality') {
+                        $output .= "&pbk={$config['pbk']}&sid={$config['sid']}";
+                    }
+                }
+
+                switch ($config['type']) {
+                    case 'tcp':
+                        $header = $node_custom_config['header'] ?? ['type' => 'none'];
+                        $config['headerType'] = $header['type'] ?? '';
+                        $output .= "&headerType={$config['headerType']}";
+                        break;
+
+                    case 'kcp':
+                        $header = $node_custom_config['header'] ?? ['type' => 'none'];
+                        $config['headerType'] = $header['type'] ?? '';
+                        $output .= "&headerType={$config['headerType']}&seed={$config['path']}";
+                        break;
+
+                    case 'ws':
+                        $output .= "&path={$config['path']}&host={$config['host']}";
+                        break;
+
+                    case 'h2':
+                        $output .= "&path={$config['path']}&host={$config['host']}";
+                        break;
+
+                    case 'quic':
+                        $header = $node_custom_config['header'] ?? ['type' => 'none'];
+                        $config['headerType'] = $header['type'] ?? '';
+                        $output .= "&quicSecurity={$config['quicSecurity']}&headerType={$config['headerType']}";
+                        if ((string) $config['quicSecurity'] !== 'none') {
+                            $output .= "&key={$config['path']}";
+                        }
+                        break;
+
+                    case 'grpc':
+                        if (isset($node_custom_config['multiMode'])) {
+                            $config['mode'] = $node_custom_config['multiMode'] ? "multi" : "gun";
+                        }
+                        $output .= "&serviceName={$config['serviceName']}&mode={$config['mode']}";
+                        break;
+                }
+
+                $output .= "&fp=chrome#" . $config['name'];
+                $links .= $output . "\r\n";
+            } else {
                 $v2_port = $node_custom_config['v2_port'] ?? ($node_custom_config['offset_port_user']
                     ?? ($node_custom_config['offset_port_node'] ?? 443));
                 //默認值有問題的請懂 V2 怎麽用的人來改一改。
@@ -282,5 +363,17 @@ final class LinkController extends BaseController
         $token = Link::where('userid', $userid)->first();
 
         return $_ENV['subUrl'] . '/link/' . $token->token;
+    }
+
+    public static function encodeURIComponent($str)
+    {
+        $revert = [
+            '%21' => '!',
+            '%2A' => '*',
+            '%27' => "'",
+            '%28' => '(',
+            '%29' => ')',
+        ];
+        return strtr(rawurlencode($str), $revert);
     }
 }
