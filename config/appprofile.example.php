5d3e3375a3be1773e0631450b7f952a4379666df
77-QiQi <87052253+77-QiQi@users.noreply.github.com>
2024-07-24 12:12:13 +0800

    Update appprofile.example.php

--- config/appprofile.example.php
+++ config/appprofile.example.php
@@ -91,9 +91,9 @@ $_ENV['SingBox_Config'] = [
     'inbounds' => [
         [
             'type' => 'tun',
-            "address" => [
-                "172.18.0.1/30",
-                "fdfe:dcba:9876::1/126",
+            'address' => [
+                '172.18.0.1/30',
+                'fdfe:dcba:9876::1/126',
                 ],
             'auto_route' => true,
             'strict_route' => true,
