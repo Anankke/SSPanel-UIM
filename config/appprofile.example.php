213f6a8caf8f15ff54abe84f2c2c0601594451cb
77-QiQi <87052253+77-QiQi@users.noreply.github.com>
2024-12-22 04:22:32 +0800

    fix: typo

--- config/appprofile.example.php
+++ config/appprofile.example.php
@@ -95,8 +95,6 @@ $_ENV['SingBox_Config'] = [
             ],
             [
                 'query_type' => [
-                    'A',
-                    'AAAA',
                     'SVCB',
                     'HTTPS',
                 ],
@@ -136,7 +134,7 @@ $_ENV['SingBox_Config'] = [
             'tag' => 'in',
             'address' => [
                 '172.18.0.1/30',
-                'fdfe:dcba:9876::1/126',
+                //'fdfe:dcba:9876::1/126',
             ],
             'auto_route' => true,
             'strict_route' => true,
